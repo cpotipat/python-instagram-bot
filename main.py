1a4dc0d426843c3dfd5c548b99081feb84658745
cpotipat <namsom.chitchanok@gmail.com>
2021-04-26 15:58:25 -0700

    Log in and search for the target account

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -7,6 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 CHROME_DRIVER_PATH = os.environ.get("CHROME_DRIVER_PATH")
 INSTAGRAM_USERNAME = os.environ.get("INSTAGRAM_USERNAME")
 INSTAGRAM_PASSWORD = os.environ.get("INSTAGRAM_PASSWORD")
+TARGET_ACCOUNT = "hawaiitraveltips"
 
 
 class InstaFollower:
@@ -14,10 +16,27 @@ class InstaFollower:
         self.driver = webdriver.Chrome(executable_path=path)
 
     def login(self):
-        pass
+        self.driver.get("https://www.instagram.com/")
+        wait = WebDriverWait(self.driver, 10)
+
+        wait.until(EC.visibility_of_element_located((By.ID, "loginForm")))
+        username = self.driver.find_element_by_name("username")
+        username.send_keys(INSTAGRAM_USERNAME)
+        password = self.driver.find_element_by_name("password")
+        password.send_keys(INSTAGRAM_PASSWORD)
+        password.send_keys(Keys.ENTER)
+        wait.until(EC.visibility_of_element_located((By.TAG_NAME, "nav")))
 
     def find_followers(self):
-        pass
+        wait = WebDriverWait(self.driver, 10)
+        search_box = wait.until(EC.visibility_of_element_located((By.TAG_NAME, "input")))
+        search_box.send_keys(TARGET_ACCOUNT)
+
+        target_account = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div[3]/div/div[2]/div/div[1]/a')))
+        target_account.click()
+
+        followers = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a')))
+        followers.click()
 
     def follow(self):
         pass
