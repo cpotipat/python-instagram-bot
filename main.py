402a1d147e6a7855ebf875bb35b6c52a8834d864
cpotipat <namsom.chitchanok@gmail.com>
2021-04-26 18:23:19 -0700

    Follow target account's followers

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import ElementClickInterceptedException
 
 CHROME_DRIVER_PATH = os.environ.get("CHROME_DRIVER_PATH")
 INSTAGRAM_USERNAME = os.environ.get("INSTAGRAM_USERNAME")
@@ -38,8 +40,20 @@ class InstaFollower:
         followers = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a')))
         followers.click()
 
+        modal = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[5]/div/div/div[2]')))
+        for i in range(5):
+            self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", modal)
+            time.sleep(2)
+
     def follow(self):
-        pass
+        all_button = self.driver.find_elements_by_css_selector("li button")
+        for button in all_button:
+            try:
+                button.click()
+                time.sleep(1)
+            except ElementClickInterceptedException:
+                cancel_button = self.driver.find_element_by_xpath('/html/body/div[6]/div/div/div/div[3]/button[2]')
+                cancel_button.click()
 
 
 bot = InstaFollower(CHROME_DRIVER_PATH)
